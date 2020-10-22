fb64ce8a5ac408bbaf934c90aaf0ae84db5b510a
Anwesh43 <anweshthecool0@gmail.com>
2020-10-23 04:47:00 +0530

    adding useStyle hooks

--- src/hooks.js
+++ src/hooks.js
@@ -3,6 +3,10 @@ import {
   useState 
 } from 'react'
 
+import {
+  divideScale, 
+  sinify
+} from './utils'
 export const useAnimatedScale = (scGap = 0.02, delay = 20) => {
     const [scale, setScale] = useState(0)
     const [animated, setAnimated] = useState(false)
@@ -38,5 +42,36 @@ export const useDimension = () => {
     return {
         w, 
         h
+    }
+}
+
+export const useStyle = (w, h, scale) => {
+    const sf = sinify(scale)
+    const sf1 = divideScale(sf, 0, 2)
+    const sf2 = divideScale(sf, 1, 2)
+    const position = 'absolute'
+    const size = Math.min(w, h) / 12 
+    const y = h - size
+    const dx = w - size 
+    const background = 'green'
+    const hFactor = 60 
+    const lineH = Math.min(w, h) / hFactor 
+    return {
+        
+        getLineStyle(i) {
+            const left = `${(w - size) * (1 - sf1) * i}px`
+            const width = `${size + (w - size) * sf1}px`
+            const height = `${lineH}px`
+            return {position, background,let, top, left, width, height}
+        },
+
+        getBarFillStyle() {
+            const barW = w * sf2
+            const width = `${barW}px`
+            const height = `${size}px`
+            const left = `${w / 2 - barW / 2}px`
+            const top = `${h - size}px`
+            return {position, background, left, top, width, height}
+        }
     }
 }
