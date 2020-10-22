c24bf9f540ec9ce5f92cb0ea29c2eac42663e9ac
Anwesh43 <anweshthecool0@gmail.com>
2020-10-23 04:59:54 +0530

    adding DoubleSideJoinLine class

--- src/DSJLComponent.js
+++ src/DSJLComponent.js
@@ -16,6 +16,7 @@ const DSJLComponent = ({scale, w, h, onClick}) => {
     } = useStyle(w, h, scale)
     return (
         <div>
+            <button onClick = {onClick}>start</button>
             {[0, 1].map(i => <Line key = {`line_${i}`} style = {getLineStyle(i)}/>)}
             <div style = {getBarFillStyle()}>
             </div>
---
+++ src/DoubleSideLineJoin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import DSLJComponent from './DSJLComponent'
+import {
+    useAnimatedScale, 
+    useDimension
+} from './hooks'
+
+const DoubleSideLineJoin = (props) => {
+    const {scale, start} = useAnimatedScale(0.02, 20)
+    const {w, h} = useDimension()
+    return <div>
+        <DSLJComponent w = {w} scale = {scale} h = {h} onClick = {start()}></DSLJComponent>
+    </div>
+}
